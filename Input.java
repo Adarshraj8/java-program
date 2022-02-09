9875c99cc540a89746cfaf0ba00190f434497d36
Adarsh Raj <adarshrajgr2016@gmail.com>
2022-02-09 23:54:38 +0530

    Add files via upload

--- Input.java
+++ Input.java
@@ -1,23 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package adarsh.decimaltobinary;
-import java.util.Scanner;
+
+package adarsh.fibonacci;
+   import java.util.Scanner;
 
 public class Input {
     
-    public static void main(String[] args){
-    int numberModule,decimalNumber;
-    String binaryNumber = "";
-    Scanner input = new Scanner(System.in);
-    System.out.println("Enter a Decimal Number :");
-    decimalNumber = input.nextInt();
-    while(decimalNumber>0){
-        numberModule = decimalNumber%2;
-        binaryNumber = numberModule + "" + binaryNumber ;
-        decimalNumber = decimalNumber/2;
+        public static void main(String[] args) 
+
+        {
+
+            int n, a = 0, b = 1,c;
+            System.out.println(a+"");
+            System.out.println(b+"");
+
+            Scanner s = new Scanner(System.in);
+
+            System.out.println("Enter value of n:");
+
+            n = s.nextInt();
+
+            System.out.println("Fibonacci Series:");
+
+            for(int i = 1; i <= n; i++)
+
+            {
+
+              
+
+                c = a + b;
+              System.out.println(c+" ");
+              a=b;
+              b=c;
+
+            }
+
+        }
+
     }
-    System.out.println("Binay Notation : "+binaryNumber);
-    }
-}
+
