04f62ed22abed43cf6a690280233f2ecc19f8ba6
Adarsh Raj <adarshrajgr2016@gmail.com>
2022-02-10 00:14:14 +0530

    Add files via upload

--- Input.java
+++ Input.java
@@ -1,39 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adarsh.largestnum;
+import java.util.Scanner;
 
-package adarsh.fibonacci;
-   import java.util.Scanner;
-
+/**
+ *
+ * @author user
+ */
 public class Input {
+    public static void main(String[] args){
     
-        public static void main(String[] args) 
-
-        {
-
-            int n, a = 0, b = 1,c;
-            System.out.println(a+"");
-            System.out.println(b+"");
-
-            Scanner s = new Scanner(System.in);
-
-            System.out.println("Enter value of n:");
-
-            n = s.nextInt();
-
-            System.out.println("Fibonacci Series:");
-
-            for(int i = 1; i <= n; i++)
-
-            {
-
-              
-
-                c = a + b;
-              System.out.println(c+" ");
-              a=b;
-              b=c;
-
-            }
-
+        int a = 100,b = 50,c=200;
+        if(a>b && a>c){
+            System.out.println("largest number is a : " + a);
         }
-
-    }
+            else if(b>a && b>c){
+                    System.out.println("largest number is b : " + b);
+                    }
+                    else{
+                    System.out.println("largest number is c: " + c);
+                    }
+        }
+}
+            
+    
+    
+    
 
